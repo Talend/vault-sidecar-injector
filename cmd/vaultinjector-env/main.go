4f8d83ebea04000201eadbb094b36db50ff33cd5
Alain Saint-Sever <asaintsever@talend.com>
2020-06-09 16:24:13 +0200

    Upd

--- cmd/vaultinjector-env/main.go
+++ cmd/vaultinjector-env/main.go
@@ -14,8 +14,111 @@
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
+	"strings"
+	"syscall"
+
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	defaultSecretsFilesPath = "/opt/talend/secrets"
+)
+
+func init() {
+	jsonLog, _ := strconv.ParseBool(os.Getenv("VSI_ENV_LOG_JSON"))
+	if jsonLog {
+		// Log as JSON instead of the default ASCII formatter
+		log.SetFormatter(&log.JSONFormatter{})
+	}
+
+	// Only log the warning severity or above
+	log.SetLevel(log.WarnLevel)
+}
 
 func main() {
-	fmt.Println("Hello")
+	var entrypointCmd []string
+	if len(os.Args) == 1 {
+		log.Fatalln("failed to determine entrypoint")
+	} else {
+		entrypointCmd = os.Args[1:]
+	}
+
+	binary, err := exec.LookPath(entrypointCmd[0])
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	env := os.Environ()
+
+	// Enrich env vars with content of all secrets files
+	secretsFilesPath := os.Getenv("VSI_ENV_SECRETS_FILES_PATH")
+	if secretsFilesPath == "" {
+		secretsFilesPath = defaultSecretsFilesPath
+	}
+
+	secretsFiles, err := ioutil.ReadDir(secretsFilesPath)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	for _, fsecrets := range secretsFiles {
+		props, err := parsePropertiesFile(path.Join(secretsFilesPath, fsecrets.Name()))
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+
+		// Add to env vars. We do not check for collisions: make sure to not have same keys in secrets files (and do not use existing env keys either)
+		env = append(env, props...)
+	}
+
+	// Replace current process with original one, providing env vars (including new ones from fetched secrets)
+	err = syscall.Exec(binary, entrypointCmd, env)
+	if err != nil {
+		log.Panicln("failed to exec process", entrypointCmd, err.Error())
+	}
+}
+
+func parsePropertiesFile(filename string) ([]string, error) {
+	var props []string
+
+	if len(filename) == 0 {
+		return props, nil
+	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if equal := strings.Index(line, "="); equal >= 0 {
+			if key := strings.TrimSpace(line[:equal]); len(key) > 0 {
+				value := ""
+				if len(line) > equal {
+					value = strings.TrimSpace(line[equal+1:])
+				}
+
+				props = append(props, fmt.Sprintf("%s=%s", key, value))
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return props, nil
 }
--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	github.com/prometheus/client_golang v0.9.3
+	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/testify v1.4.0
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
