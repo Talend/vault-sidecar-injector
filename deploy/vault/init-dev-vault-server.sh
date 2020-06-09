830f9cac71cd2e57ccd01ecd1d9143b56eea0b8f
Alain Saint-Sever <asaintsever@talend.com>
2020-06-09 16:43:38 +0200

    Upd

--- deploy/vault/init-dev-vault-server.sh
+++ deploy/vault/init-dev-vault-server.sh
@@ -8,6 +8,7 @@ VAULT_POD="kubectl exec -i vault-0 -- sh -c"
 # Create policies to allow read access to our secrets
 cat "$SCRIPT_PATH"/vault-server-policy-test.hcl | ${VAULT_POD} "VAULT_TOKEN=root vault policy write test_pol -"
 cat "$SCRIPT_PATH"/vault-server-policy-test2.hcl | ${VAULT_POD} "VAULT_TOKEN=root vault policy write test_pol2 -"
+cat "$SCRIPT_PATH"/vault-server-policy-test3.hcl | ${VAULT_POD} "VAULT_TOKEN=root vault policy write test_pol3 -"
 
 # Enable KV v1 (KV v2 is enabled with Vault server in dev mode whereas KV v1 is enabled in prod mode: https://www.vaultproject.io/docs/secrets/kv/kv-v2.html#setup)
 ${VAULT_POD} "VAULT_TOKEN=root vault secrets disable secret/"
@@ -31,6 +32,7 @@ ${VAULT_POD} "VAULT_TOKEN=root vault write auth/kubernetes/config kubernetes_hos
 # Create roles for Vault K8S Auth Method
 ${VAULT_POD} "VAULT_TOKEN=root vault write auth/kubernetes/role/test bound_service_account_names=default,job-sa bound_service_account_namespaces=default policies=test_pol ttl=5m"
 ${VAULT_POD} "VAULT_TOKEN=root vault write auth/kubernetes/role/test2 bound_service_account_names=default,job-sa bound_service_account_namespaces=default policies=test_pol2 ttl=5m"
+${VAULT_POD} "VAULT_TOKEN=root vault write auth/kubernetes/role/test3 bound_service_account_names=default,job-sa bound_service_account_namespaces=default policies=test_pol3 ttl=5m"
 
 # Enable Vault AppRole Auth Method
 echo "-> Enable & set up Vault AppRole Auth Method"
@@ -39,10 +41,13 @@ ${VAULT_POD} "VAULT_TOKEN=root vault auth enable approle" || true
 # Create roles for Vault AppRole Auth Method
 ${VAULT_POD} "VAULT_TOKEN=root vault write auth/approle/role/test secret_id_ttl=60m token_num_uses=0 token_ttl=20m token_max_ttl=30m secret_id_num_uses=0 policies=test_pol"
 ${VAULT_POD} "VAULT_TOKEN=root vault write auth/approle/role/test2 secret_id_ttl=60m token_num_uses=0 token_ttl=20m token_max_ttl=30m secret_id_num_uses=0 policies=test_pol2"
+${VAULT_POD} "VAULT_TOKEN=root vault write auth/approle/role/test3 secret_id_ttl=60m token_num_uses=0 token_ttl=20m token_max_ttl=30m secret_id_num_uses=0 policies=test_pol3"
 
 # Add some secrets
 ${VAULT_POD} "VAULT_TOKEN=root vault kv put secret/test/test-app-svc ttl=10s SECRET1=Batman SECRET2=BruceWayne"
 ${VAULT_POD} "VAULT_TOKEN=root vault kv put secret/test2/test-app2-svc ttl=5s SECRET1=my SECRET2=name SECRET3=is SECRET4=James"
+${VAULT_POD} "VAULT_TOKEN=root vault kv put secret/test3/test-app3-svc1 ttl=15s SECRET1=svc1_sec1_value SECRET2=svc1_sec2_value"
+${VAULT_POD} "VAULT_TOKEN=root vault kv put secret/test3/test-app3-svc2 ttl=15s SECRET1=svc2_sec1_value SECRET2=svc2_sec2_value"
 
 # List Auth Methods and Secrets Engines
 echo
